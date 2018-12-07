df3817b15587f1f21ecc9d918fa0c72a5a9c26ea
Travis Harris <txh@AIMR.ORG>
2018-12-07 11:03:53 -0500

    Added Day3 Solution

---
+++ Day3/Fabric.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    public class Fabric
+    {
+        //#1 @ 872,519: 18x18
+        public string Input { get; set; }
+
+        public int XCoord { get; set; }
+
+        public int YCoord { get; set; }
+
+        public int XCoordOffset { get; set; }
+
+        public int YCoordOffset { get; set; }
+
+        public int FabricSize { get; set; }
+
+
+        public Fabric(string input)
+        {
+            input = input.Replace(',', ' ');
+            input = input.Replace('@', ' ');
+            input = input.Replace(':', ' ');
+            input = input.Replace('x', ' ');
+            var escapedInput = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            this.Input = escapedInput[0];
+            this.XCoord = Int32.Parse(escapedInput[1]);
+            this.YCoord = Int32.Parse(escapedInput[2]);
+            this.XCoordOffset = Int32.Parse(escapedInput[3]) + XCoord - 1;
+            this.YCoordOffset = Int32.Parse(escapedInput[4]) + YCoord - 1;
+            this.FabricSize = XCoordOffset * YCoordOffset;
+
+            var a = "b";
+        }
+    }
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rawInput = File.ReadAllText("../../../Input/day3Input.txt");
+            var inputLines = rawInput.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+            //var inputLines = new String[] { "#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2" };
+
+            string[,] array = new string[1000, 1000];
+            List<Fabric> fabrics = new List<Fabric>();
+
+            foreach (var s in inputLines)
+            {
+                Fabric f = new Fabric(s);
+                fabrics.Add(f);
+                for (int row = (f.YCoord); row <= f.YCoordOffset; row++)
+                {
+                    for (int col = (f.XCoord); col <= f.XCoordOffset; col++)
+                    {
+                        array[row, col] += f.Input;
+                    }
+
+                }
+            }
+
+
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    var test = array[i, j];
+                    if (test != null)
+                    {
+                        if (test.Count(a => a == '#') > 1)
+                        {
+                            var fabricToRemove = test.Split(new char[] { '#' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                            foreach (var d in fabricToRemove)
+                            {
+                                fabrics.Remove(fabrics.SingleOrDefault(z => z.Input == ("#" + d)));
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(fabrics.FirstOrDefault().Input);
+
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    for (int j = 0; j < 8; j++)
+            //    {
+            //        var whatToWrite = GetIt(array[i, j]);
+            //        Console.Write(whatToWrite);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+            var multiCount = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    if (array[i, j] != null)
+                    {
+                        if (array[i, j].Count(a => a == '#') > 1)
+                            multiCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Multicount = " + multiCount);
+
+
+        }
+
+        public static string GetIt(string s)
+        {
+            if (s == null)
+                return "@";
+            if (s.Count(a => a == '#') > 1)
+                return "M";
+            if (s.Count(a => a == '#') == 1)
+            {
+                return s.Replace('#', ' ').Trim();
+            }
+
+            return "%";
+        }
+
+
+
+        //#1 @ 872,519: 18x18
+    }
+}
